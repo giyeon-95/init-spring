16d60ca8a210410ba5f231a5311946006a51ffb5
h.jhkim@kalmate.net <h.jhkim@kalmate.net>
2024-06-04 14:50:51 +0900

    [Spring Container & Bean] : tutorial 1 branch commit

--- src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -10,6 +10,7 @@ public class App03GamingSpringBeans {
 
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+            //
             context.getBean(GamingConsole.class).up();
 
             context.getBean(GameRunner.class).run();

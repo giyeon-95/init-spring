d5d29c189fa4b35d2c5a0e5000c4ff625e02c70f
h.jhkim@kalmate.net <h.jhkim@kalmate.net>
2024-06-04 16:02:20 +0900

    [Spring Container & Bean] : @Primary / @Qualifier 로 클래스 우선 지정

--- src/main/java/com/in28minutes/learn_spring_framework/GamingAppLauncherApp.java
+++ src/main/java/com/in28minutes/learn_spring_framework/GamingAppLauncherApp.java
@@ -8,24 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.in28minutes.learn_spring_framework.game")
 public class GamingAppLauncherApp {
 
-
-    /* Bean 자동생성 ( pacmanGame instance / GameRunner instance 자동생성 ) 
-     * 아래 Bean을 제거하고, PacmanGame Class / GameRunner Class에 @Component를 설정한다.
-     * PacmanGame Class / GameRunner Class 가 위치하는 경로를 ComponentScan에 추가한다.
-     * 이로써 Spring은 자동으로 패키지를 스캔하고, 컴포넌트를 찾고, 인스턴스를 생성하고, 의존성을 주입한다.
-    */
-
-    // @Bean
-    // public GamingConsole game() {
-    //     var game = new PacmanGame();
-    //     return game;
-    // }
-
-    // @Bean
-    // public GameRunner gameRunner(GamingConsole game) {
-    //     var gameRunner = new GameRunner(game);
-    //     return gameRunner;
-    // }
+    /* 다중 @Component class 발생시, @Primary 우선 Scan. */
 
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApp.class)) {
--- src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,21 +1,14 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
-
-    /*
-     * 결합의 정도
-     * 
-     * 느슨한 결합 -> GameRunner 클래스가 어떠한 game 클래스를 사용할 때, 별도의 인스턴스를 생성하지 않고 생성자를 통해
-     * 전달받는다. (GameRunner 클래스 생성자만 수정필요)
-     * 강한 결합 -> GameRunner 클래스가 어떠한 game 클래스를 사용할 때, 클래스의 인스턴스를 추가적으로 생성한다.
-     * (GameRunner 클래스 생성자와 run 메소드 수정필요)
-     */
-
     private GamingConsole game;
 
+    /* @Qualifier class 가 있는경우, @Qualifier로 class를 특정할 수 있다. */
+    // public GameRunner(@Qualifier("superContraQualifier") GamingConsole game) {
     public GameRunner(GamingConsole game) {
         this.game = game;
     }
